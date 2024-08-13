c7bc38da36f81c09a181800ae6df5150f70e92b6
AROEDISANALWA C <153987228+Edisan252@users.noreply.github.com>
2024-08-13 11:32:40 +0530

    Update portfolio.jsx

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -93,7 +93,7 @@ const Port = () =>{
                     onMouseLeave={() => setHovered(false)}
                     onMouseMove={handleMouseMove}>
                     <div className="image-wrapper1">
-                        <img src="../edisan.jpg" alt="Your Image" style={{ transformOrigin: `${position.x * 100}% ${position.y * 100}%` }} /></div>
+                        <img src="../edisan.jpg" alt="" style={{ transformOrigin: `${position.x * 100}% ${position.y * 100}%` }} /></div>
                      </div>
                     </div>
                     <div className="col-7">
@@ -191,7 +191,7 @@ const Port = () =>{
         <div className="row">
             <div className="col-4">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a9'>
-      <img src='../backery.png' className='w-100' />
+      <img src='../backery.png' alt="" className='w-100' />
       <a href='https://regal-khapse-b430f9.netlify.app'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -199,7 +199,7 @@ const Port = () =>{
         </div>
         <div className="col-4">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a10'>
-      <img src='../calculator.png'  />
+      <img src='../calculator.png'  alt=""/>
       <a href='https://bucolic-gnome-223ec1.netlify.app/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -207,7 +207,7 @@ const Port = () =>{
         </div>
         <div className="col-4">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a10'>
-      <img src='../guess.png'  />
+      <img src='../guess.png'  alt=""/>
       <a href='https://cosmic-monstera-7b15f1.netlify.app/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -215,7 +215,7 @@ const Port = () =>{
         </div>
         <div className="col-4 mt-5">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a10'>
-      <img src='../1.jpeg'  />
+      <img src='../1.jpeg' alt="" />
       <a href='#4'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -223,7 +223,7 @@ const Port = () =>{
         </div>
         <div className="col-4 mt-5">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a10'>
-      <img src='../medical.png'  />
+      <img src='../medical.png' alt="" />
       <a href='#5'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -231,7 +231,7 @@ const Port = () =>{
         </div>
         <div className="col-4 mt-5">
             <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow a10'>
-      <img src='../vetnary.png'  />
+      <img src='../vetnary.png' alt="" />
       <a href='#6'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)',borderRadius:'10px'}}></div>
       </a>
@@ -241,32 +241,32 @@ const Port = () =>{
         <div className="caro mt-5">
         <Carousel>
       <Carousel.Item interval={1000}>
-        <img src="../calculator.png"/>
+        <img src="../calculator.png" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <img src="../backery.png"/>
+      <img src="../backery.png" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <img src="../guess.png"/>
+      <img src="../guess.png" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <img src="../medical.png"/>
+      <img src="../medical.png" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <img src="../vetnary.png"/>
+      <img src="../vetnary.png" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <img src="../1.jpeg"/>
+      <img src="../1.jpeg" alt=""/>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
@@ -282,10 +282,10 @@ const Port = () =>{
             <div className="row">
                 <div className="col-4 a13 a5 mt-5 a11">
                     <h2><a href="#"><FaEnvelope /> Email Id:<br/><span>aroedisanawack@gmail.com</span></a></h2>
-                    <a href=""><h2><MdLocationPin /> Location:<br/><span>6/250/5,mamarathupatti,</span></h2>
+                    <a href="#"><h2><MdLocationPin /> Location:<br/><span>6/250/5,mamarathupatti,</span></h2>
                         <h2><span>Lembalakkudi(PO),Thirumayam(TK),</span></h2>
                         <h2><span>Pudukkottai(DT).622412</span></h2></a>
-                    <h2><a href=""><MdCall /> Contact:<br/><span>+91 8838431399</span></a></h2>
+                    <h2><a href="#"><MdCall /> Contact:<br/><span>+91 8838431399</span></a></h2>
                     <ul>
                     <li><a href="https://www.instagram.com/king_of_pdkt_55?utm_source=qr&igsh=MW16amh0dTJ5YWQzag== "><FaInstagram /></a></li>
                     <li><a href=" https://wa.me/qr/2RQKORRT2GQXB1"><FaWhatsapp/></a></li>
@@ -294,7 +294,7 @@ const Port = () =>{
                 </ul>
                 </div>
             <div className="col-4 a12">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15697.925625386833!2d78.7898292445985!3d10.38328633587952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00782d25029679%3A0x4503e2d3b4a3897f!2z4K6q4K-B4K6k4K-B4K6V4K-N4K6V4K-L4K6f4K-N4K6f4K-ILCDgrqTgrq7grr_grrTgr40g4K6o4K6-4K6f4K-BIDYyMjAwMw!5e0!3m2!1sta!2sin!4v1719571039827!5m2!1sta!2sin" width="600" height="450" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15697.925625386833!2d78.7898292445985!3d10.38328633587952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00782d25029679%3A0x4503e2d3b4a3897f!2z4K6q4K-B4K6k4K-B4K6V4K-N4K6V4K-L4K6f4K-N4K6f4K-ILCDgrqTgrq7grr_grrTgr40g4K6o4K6-4K6f4K-BIDYyMjAwMw!5e0!3m2!1sta!2sin!4v1719571039827!5m2!1sta!2sin" width="600" height="450" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="a"></iframe>
                 </div>
                 <div className="col-4 a5 a7">
                 <div className="contact-form-container">
